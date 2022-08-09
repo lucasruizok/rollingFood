1ae0d92d3be61d7b3192266fcdc73e3231d05c2c
flopezpondaldev <flopezpondal.dev@gmail.com>
2022-08-09 16:26:16 -0300

    structure login

--- rollingFood/src/layout/login/Login.jsx
+++ rollingFood/src/layout/login/Login.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <header className='Login-header'>
+        <Form autoComplete="off" labelCol={{ span: 10 }} wrapperCol={{ span: 14 }}>
+          // Input: Email
+          <Form.Item name="email" label="Email" rules={[{
+            required: true,
+            message: "Please enter you email"
+          }]}>
+            <Input placeholder="Type your email" />
+          </Form.Item>
+          // Input: Password
+          <Form.Item name="password" label="Password">
+            <Input.Password placeholder="Type your password" />
+          </Form.Item>
+          // Checkbox: Remember Password
+          <Form.Item name="remember" wrapperCol={{ span: 24 }}>
+            <Checkbox>
+              {" "}
+              Remember my password
+            </Checkbox>
+          </Form.Item>
+          // Button: Log In
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">Log In</Button>
+          </Form.Item>
+        </Form>
+      </header>
+    </div>
   )
 }
