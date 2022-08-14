b5f8ad72aea5d6f552495bc5747e426f4d6887fd
lucasruizok <lucaruiz734@gmail.com>
2022-08-14 19:01:31 -0300

    modal

--- rollingFood/src/App.jsx
+++ rollingFood/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 //componentes Ant Desing
 const { Header, Footer, Content } = Layout;
--- rollingFood/src/layout/admin/AdminUsers.jsx
+++ rollingFood/src/layout/admin/AdminUsers.jsx
@@ -1,49 +1,35 @@
 import React, { useState } from 'react'
 import { Switch, Table, Tag, Space } from 'antd';
 import { ModalAdmin } from './ModalAdmin';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const onChange = (e) => {
-  console.log(`switch to ${e}`);
-  if(e === false){
-    <>
-    <p>hola</p>
-    </>
-  }
-  
-};
+export const AdminUsers = () => {  
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    title: 'Avatar',
+    dataIndex: 'avatar',
+    render: (imgAvatar) => <img src={imgAvatar}/>
   },
   {
-    title: 'Age',
+    title: 'Nombre de Usuario',
+    dataIndex: 'nameUser',
+  },
+  {
+    title: 'Edad',
     dataIndex: 'age',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
+    title: 'Email',
+    dataIndex: 'mail',
   },
   {
     title: 'Estado',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag === 'activo' ? 'green' : 'red';
-          console.log(tag);
-          return (
-            <>
-            <Switch defaultChecked size='small' onChange={onChange} />
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-            </>
-          );
-        })}
-      </>
-    ),
+    dataIndex: 'state',
+  },
+  {
+    title: 'Rol',
+    dataIndex: 'role'
   },
   {
     title: 'Acciones',
@@ -55,30 +41,24 @@ const columns = [
     ),
   },
 ];
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['activo']
-  },
-  {
-    key: '1',
-    name: 'hola',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['activo']
-  },
-]
+const URL = 'http://localhost:3400'
+const [data, setData] = useState([]);
 
+useEffect(function(){
+  console.log('useEfect funcionando');
+  getUsers()
+},[])
 
-export const AdminUsers = () => {
-
+async function getUsers(){
+      const res = await axios.get(URL+'/users');
+      const usersDB = res.data.users
+      setData(usersDB)
+}
   return (
 
     <div className='container'>
       <h4>LISTA DE USUARIOS</h4>
+      <ModalAdmin/>
       <hr />
       <Table columns={columns} dataSource={data} size="middle" />
     </div>
